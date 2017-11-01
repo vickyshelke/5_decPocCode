7affe113ed1a55317b402c38994b3648a6c05220
vikramshelke <vickyshelke95@gmail.com>
2017-11-01 21:53:36 +0530

    Create buffer.py
    circular buffer for storing data locally in case of no network connection or not able to send data.

---
+++ buffer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       fifo(.py)
+#
+import os
+FILENAME="BUFFER"
+SIZE=5
+def string_conditioned(string):
+    return string.decode('string_escape').rstrip() + '\n'
+def pop():
+    if os.stat(FILENAME).st_size > 0:
+        with open(FILENAME, 'r+U') as fd:
+            rows = fd.readlines()
+        with open(FILENAME, 'w') as fd:
+            n = int(1)
+            fd.writelines(rows[n:])
+            return ''.join(rows[:n])
+    else:
+        return "buffer empty"
+def trim_buffer(row):
+    size = int(SIZE)
+    with open(FILENAME, 'rU') as fd:
+        rows = fd.readlines()
+    num_rows = len(rows)
+    if num_rows >= size:
+        n = string_conditioned(row).count('\n')
+#        pop(num_rows + n - size)
+        pop()
+def push(row):
+    trim_buffer(row)
+    with open(FILENAME, 'a') as fd:
+        fd.write(string_conditioned(row))
+    return ''
+#Use push("shelke") print (pop().rstrip())
